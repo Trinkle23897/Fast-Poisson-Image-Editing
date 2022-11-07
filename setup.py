6a607f17e335a8be97cbfe39dd9b2de6de2a7cb6
Jiayi Weng <trinkle23897@gmail.com>
2022-11-06 20:15:31 -0800

    Catch all exceptions when installing

--- setup.py
+++ setup.py
@@ -52,7 +52,8 @@ class CMakeBuild(build_ext):
         ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
       )
       subprocess.check_call(["make"] + build_args, cwd=self.build_temp)
-    except subprocess.CalledProcessError:
+    except Exception as e:
+      print(f"{type(e)}: {e}")
       pass
 
 
