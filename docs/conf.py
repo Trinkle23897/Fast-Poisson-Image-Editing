a7215373e76eb2ff3cf06adb199a64eb73bb905d
Jiayi Weng <jiayi@openai.com>
2026-03-20 08:03:10 -0700

    fix: drop deprecated sphinx theme path config

--- docs/conf.py
+++ docs/conf.py
@@ -16,8 +16,6 @@
 
 import os
 
-import sphinx_rtd_theme
-
 # -- Project information -----------------------------------------------------
 
 project = "fpie"
@@ -63,7 +61,6 @@ spelling_exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
